5c008972be5453568ad8a7d0db8e67c6504a3a81
Niraj Tarway <nirajntr@gmail.com>
2023-01-14 20:21:31 +0530

    template filepath bug resolved

--- template.py
+++ template.py
@@ -32,13 +32,15 @@ list_of_fies = [
 ]
 
 for filepath in list_of_fies:
+    #break
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
     if filedir!='':
-        os.makedirs(filedir,exist_ok=True)
-        logging.info(f"Creating directory {filedir} for file {filename}")
+         if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+            os.makedirs(filedir,exist_ok=True)
+            logging.info(f"Creating directory {filedir} for file {filename}")
     
-    if(not os.path.exists(filename)) or (os.path.getsize(filepath)==0):
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
         with open(filepath,"w") as f:
             pass # creating empty directory
             logging.info(f"Creating empty file: {filepath}")
