bd31d27ab5b154d914985a5a525f1565b9800027
Niraj Tarway <nirajntr@gmail.com>
2023-01-14 09:50:44 +0530

    basic skeleton ready

---
+++ template.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s')
+
+project_name = 'deepClassifier'
+
+list_of_fies = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "configs/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "research/trials.ipynb"
+]
+
+for filepath in list_of_fies:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir!='':
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory {filedir} for file {filename}")
+    
+    if(not os.path.exists(filename)) or (os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass # creating empty directory
+            logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"File: {filename} already exists.")
