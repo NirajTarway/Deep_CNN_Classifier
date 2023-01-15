2261c0f88ea74a8bd2848c90400c82e71add1605
Niraj Tarway <nirajntr@gmail.com>
2023-01-15 18:44:10 +0530

    tox related changes

--- init_setup.sh
+++ init_setup.sh
@@ -4,6 +4,6 @@ conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activating the environment" 
 source activate ./env
 echo [$(date)]: "installing the dev requirements" 
-# source activate ./env this is not working so commented below line
+# source activate ./env this is not working so commented
 # pip install -r requirements_dev.txt  
 echo [$(date)]: "END" 
